33da02ed84f32d8390d5bb590227b188095fa1ce
MikhailShatz <Tipavideo@gmail.com>
2023-09-08 10:30:19 +0300

    Exceptions and more oop

---
+++ src/main/java/org/example/ExceptionExample.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+//         throws Interrupted exception
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        try {
+            System.out.println("9/3");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getClass());
+        }finally {
+            System.out.println("finally");
+        }
+    }
+}
